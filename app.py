a776998ed21ecaa072397df00a4714e2f9e76a05
sanjeevrana90 <sanjeevrana0790@gmail.com>
2023-06-21 18:55:35 +0530

    This is a commit for web application

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def predict():
     final_input = scalar.transform(np.array(data).reshape(1, -1))
     print(final_input)
     output = regmodel.predict(final_input)[0]
-    return  render_template("home.html", prediction_text = "The House Price Prediction is {}".format(output))
+    return  render_template("home.html", prediction_text = "The House Price Prediction is (in Lacs) {}".format(output))
 
 
 
